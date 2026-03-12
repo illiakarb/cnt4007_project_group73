9f51048bf5660fda24719435a5650255a70887a1
Illia Karbivnychyi <karbivnychyillia@gmail.com>
2026-03-12 15:01:03 -0400

    Increased code readability for processFile_3.py

--- processFile_3.py
+++ processFile_3.py
@@ -1,11 +1,21 @@
 def read_piece(piece_id, piece_sz, f_name, peer_id):
-    path = f"peer_{peer_id}/{f_name}"
-    with open(path, 'rb') as f:
-        f.seek(piece_id * piece_sz)
-        return f.read(piece_sz)
+    folder = f"peer_{peer_id}"
+    path = folder + "/" + f_name
+    offset = piece_id * piece_sz
+    
+    file = open(path, 'rb')
+    file.seek(offset)
+    data = file.read(piece_sz)
+    file.close()
+    
+    return data
 
 def write_piece(piece_id, piece_data, piece_sz, f_name, peer_id):
-    path = f"peer_{peer_id}/{f_name}"
-    with open(path, 'r+b') as f:
-        f.seek(piece_id * piece_sz)
-        f.write(piece_data)
+    folder = f"peer_{peer_id}"
+    path = folder + "/" + f_name
+    offset = piece_id * piece_sz
+    
+    file = open(path, 'r+b')
+    file.seek(offset)
+    file.write(piece_data)
+    file.close()
